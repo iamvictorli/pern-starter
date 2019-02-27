59a580b743759231a633e594898cd3d69df91070
Victor Li <victorli5674@yahoo.com>
2019-02-27 10:25:28 -0800

    rewrite supertest

--- packages/server/__tests__/routes/librariesRoute.test.js
+++ packages/server/__tests__/routes/librariesRoute.test.js
@@ -2,26 +2,25 @@ import request from 'supertest';
 import app from '../../src/index';
 
 describe('Libraries Route', () => {
-  test('Responds with all the libraries', async done => {
-    const response = await request(app)
+  test('Responds with all the libraries', done => {
+    request(app)
       .get('/api/libraries')
       .expect('Content-Type', /json/)
-      .expect(200);
-    expect(response.body).toEqual({
-      libraries: [
-        { id: 1, libraryname: 'Express' },
-        { id: 2, libraryname: 'pg' },
-        { id: 3, libraryname: 'React' },
-        { id: 4, libraryname: 'Redux' },
-        { id: 5, libraryname: 'Redux Thunk' },
-        { id: 6, libraryname: 'React Router Dom' },
-        { id: 7, libraryname: 'ESLint' },
-        { id: 8, libraryname: 'Babel' },
-        { id: 9, libraryname: 'Jest' },
-        { id: 10, libraryname: 'Supertest' },
-        { id: 11, libraryname: 'Enzyme' }
-      ]
-    });
-    done();
+      .expect(200, {
+        libraries: [
+          { id: 1, libraryname: 'Express' },
+          { id: 2, libraryname: 'pg' },
+          { id: 3, libraryname: 'React' },
+          { id: 4, libraryname: 'Redux' },
+          { id: 5, libraryname: 'Redux Thunk' },
+          { id: 6, libraryname: 'React Router Dom' },
+          { id: 7, libraryname: 'ESLint' },
+          { id: 8, libraryname: 'Babel' },
+          { id: 9, libraryname: 'Jest' },
+          { id: 10, libraryname: 'Supertest' },
+          { id: 11, libraryname: 'Enzyme' }
+        ]
+      })
+      .end(done);
   });
 });
